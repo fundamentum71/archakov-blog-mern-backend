640ed45b9c69fefb6f6f0e51066f46b038fda78c
Aleksandr <it.kucherov@gmail.com>
2022-10-17 16:50:48 +0300

    начало бэкенда

---
+++ index.js
@@ -0,0 +1,24 @@
+import express from 'express';
+
+const app = express();
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+	res.send('Hello World!');
+});
+
+app.post('/auth/login', (req, res) => {
+	console.log(req.body);
+	res.json({
+		success: true,
+	});
+});
+
+app.listen(3000, (err) => {
+	if (err) {
+		return console.log(err);
+	}
+
+	console.log('Server OK');
+});
