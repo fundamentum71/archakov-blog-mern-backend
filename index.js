8fec7c3fba9e14b8c615afaf775b7b3aacb6dda5
Aleksandr <it.kucherov@gmail.com>
2022-10-17 19:40:02 +0300

    mongoDB ok!

--- index.js
+++ index.js
@@ -1,17 +1,34 @@
 import express from 'express';
+import jwt from 'jsonwebtoken';
+import mongoose from 'mongoose';
+
+mongoose
+	.connect('mongodb+srv://admin:wwwwww@cluster0.rxaypsn.mongodb.net/?retryWrites=true&w=majority')
+	.then(() => console.log('DB ok'))
+	.catch((err) => console.log('DB error', err));
 
 const app = express();
 
 app.use(express.json());
 
 app.get('/', (req, res) => {
-	res.send('Hello World!');
+	res.send('Привет, Аня');
 });
 
 app.post('/auth/login', (req, res) => {
 	console.log(req.body);
+
+	const token = jwt.sign(
+		{
+			email: req.body.email,
+			fullname: 'Вася Пупкин',
+		},
+		'secret123',
+	);
+
 	res.json({
 		success: true,
+		token,
 	});
 });
 
